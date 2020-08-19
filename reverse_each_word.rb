fe30f54ac0c275d8fdba28880a59d0d0fc5d5057
Bryce Ancheta <bryceancheta@gmail.com>
2020-08-19 20:23:35 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(string)
+  reg_array = string.split(" ")
+  returned_array = []
+  reg_array.each do |string|
+    returned_array << string.reverse
+  end
+  returned_array.join(" ")
+end
+  
+  
+  def reverse_each_word(string)
+    array = string.split(" ")
+    new_array = []
+    array
+  
